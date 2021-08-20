af4450050e8021993f8fc1bd89bb70ac85f0207f
Nithin <nithinjayan@gmail.com>
2021-08-20 12:05:28 +0530

    4-R2dbc test for repo and entity added

--- src/main/java/com/store/book/repos/BookRepository.java
+++ src/main/java/com/store/book/repos/BookRepository.java
@@ -1,9 +1,14 @@
 package com.store.book.repos;
 
 import com.store.book.entity.Book;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
-public interface BookRepository extends ReactiveCrudRepository<Long, Book> {
+public interface BookRepository extends ReactiveCrudRepository<Book, Long> {
+
+    @Query("SELECT * FROM book WHERE name = :name")
+    Flux<Book> findByName(String name);
 }
